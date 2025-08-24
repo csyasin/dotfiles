9f8329ae9c7b301ef44ebd2c2ebc432e2a36576a
yasin <yasin.xu@icloud.com>
2025-08-24 17:02:08 +0800

    lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,5 +4,14 @@ return {
     -- opts.options.section_separators = { left = "▓▒░", right = "░▒▓" }
     opts.options.section_separators = { left = "", right = "" }
     opts.options.component_separators = { left = "", right = "" }
+    opts.sections.lualine_a = { { "mode", separator = { left = "", right = "" } } }
+    opts.sections.lualine_z = {
+      {
+        function()
+          return " " .. os.date("%R")
+        end,
+        separator = { left = "", right = "" },
+      },
+    }
   end,
 }
